d6ba1b76540ef6ff624f88a6b22e7e37f8e437e2
João Debussy <social.debussy@gmail.com>
2023-06-08 17:51:37 -0300

    mobile(feat): create Header and Categories components

---
+++ mobile/src/Main/styles.ts
@@ -0,0 +1,26 @@
+import { Platform, StatusBar } from 'react-native';
+import styled from 'styled-components/native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Container = styled.SafeAreaView`
+  background: #FAFAFA;
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  flex: 1;
+`;
+
+export const CategoriesContainer = styled.View`
+  height: 73px;
+  margin-top: 32px;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  background: #fff;
+  min-height: 110px;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
---
+++ mobile/src/components/Categories/styles.ts
@@ -0,0 +1,21 @@
+import { Platform } from 'react-native';
+import styled from 'styled-components/native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Category = styled.TouchableOpacity`
+  align-items: center;
+  margin-left: 24px;
+`;
+
+export const Icon = styled.View`
+  background: #fff;
+  box-shadow: 0px 2px 1px rgba(0, 0, 0, ${isAndroid ? 1 : 0.1});
+  border-radius: 22px;
+  width: 44px;
+  height: 44px;
+  margin-bottom: 8px;
+  elevation: 2;
+  align-items: center;
+  justify-content: center;
+`;
