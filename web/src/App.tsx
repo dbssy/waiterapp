1c28699fad72934a77c6381d969a0956b6e1cf0d
João Debussy <social.debussy@gmail.com>
2023-06-08 14:18:33 -0300

    frontend(styles): create global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,11 @@
+import { GlobalStyles } from '@/styles/GlobalStyles';
+
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <GlobalStyles />
+
+      <h1>Hello World</h1>
+    </>
   );
 }
